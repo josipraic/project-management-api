9dbbbc3bf18e210142b341c033c853b72af987dd
Josip Raic <raicjosip39@gmail.com>
2026-02-15 23:12:53 +0100

    added project entity

---
+++ src/main/java/com/josipraic/project_management_api/entity/Project.java
@@ -0,0 +1,43 @@
+package com.josipraic.project_management_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "procts")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private ProjectStatus status;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    void prePersist() {
+        if (status == null) {
+            status = ProjectStatus.DRAFT;
+        }
+    }
+}
