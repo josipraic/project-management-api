bd4a663786c7983539df1b5cb31a41e12dfb73ed
Josip Raic <raicjosip39@gmail.com>
2026-02-17 11:03:24 +0100

    add ProjectService with business logic

---
+++ src/main/java/com/josipraic/project_management_api/service/ProjectService.java
@@ -0,0 +1,33 @@
+package com.josipraic.project_management_api.service;
+
+import com.josipraic.project_management_api.dto.ProjectCreateRequest;
+import com.josipraic.project_management_api.dto.ProjectResponse;
+import com.josipraic.project_management_api.entity.Project;
+import com.josipraic.project_management_api.mapper.ProjectMapper;
+import com.josipraic.project_management_api.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public ProjectResponse create(ProjectCreateRequest request) {
+        if (projectRepository.existsByNameIgnoreCase(request.name())) {
+            throw new IllegalArgumentException("Project with this name allready exists");
+        }
+
+        Project project = ProjectMapper.toEntity(request);
+        Project saved = projectRepository.save(project);
+
+        return ProjectMapper.toResponese(saved);
+    }
+
+    public void delete(Long id) {
+        if(!projectRepository.existsById(id)) {
+            throw new IllegalArgumentException("Project not found");
+        }
+        projectRepository.deleteById(id);
+    }
+}
